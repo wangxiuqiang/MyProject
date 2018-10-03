7a42e6a561af4c83654ceb53e99c82fdd28fd16a
wangxiuqiang <wangxiuqianga@163.com>
2018-10-03 12:08:24 +0800

    提交

---
+++ FileSystem/src/main/java/cn/fm/pojo/Level.java
@@ -0,0 +1,22 @@
+package cn.fm.pojo;
+
+public class Level {
+    private int lid;
+    private String lname;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+}
