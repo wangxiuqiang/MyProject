7c521516eea0ba919fc6c8f0ca330cd9bfed5b33
wangxiuqiang <wangxiuqianga@163.com>
2018-10-16 21:13:01 +0800

    提交

---
+++ FileSystem/src/main/java/cn/fm/pojo/Fingerprint.java
@@ -0,0 +1,33 @@
+package cn.fm.pojo;
+
+public class Fingerprint {
+    private int uid;
+    private int fid;
+    private String finger;
+
+    public String getFinger() {
+        return finger;
+    }
+
+    public void setFinger(String finger) {
+        this.finger = finger;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getFid() {
+        return fid;
+    }
+
+    public void setFid(int fid) {
+        this.fid = fid;
+    }
+
+
+}
---
+++ FileSystem/src/main/java/cn/fm/vo/FingerUser.java
@@ -0,0 +1,19 @@
+package cn.fm.vo;
+
+import cn.fm.pojo.User;
+
+public class FingerUser {
+
+
+    private User user;
+
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
