f4a59d0dcdd241c1beec96303a427a252b27beea
wangxiuqiang <wangxiuqianga@163.com>
2018-10-22 15:05:32 +0800

    单项和多项查询修改

--- FileSystem/src/main/java/cn/fm/user/dao/UserGetFileMapper.java
+++ FileSystem/src/main/java/cn/fm/user/dao/UserGetFileMapper.java
@@ -99,7 +99,7 @@ public interface UserGetFileMapper {
      * @param getFile
      * @return
      */
-    public List<GetFile> selectGetFileByTwoAndMore( GetFile getFile ,@Param(value = "endtime") String endtime ) throws Exception;
+    public List<GetFile> selectGetFileByTwoAndMore(@Param(value = "getFile") GetFile getFile ,@Param(value = "endtime") String endtime ) throws Exception;
 
     /**
      * 通过 查询是不是有借阅信息，决定是不是删除
