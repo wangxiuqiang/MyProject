e098b0c538e5364802dab373fceec76ee6ff3019
wangxiuqiang <wangxiuqianga@163.com>
2019-02-25 19:26:31 +0800

    删除

--- FileSystem/src/main/java/cn/fm/user/service/UserRegService.java
+++
@@ -1,8 +0,0 @@
-//package cn.fm.user.service;
-//
-//public interface UserRegService {
-//    /**
-//     * 设置密码
-//     */
-//    public int updateUserpwd(String upwd,String code) throws Exception;
-//}
